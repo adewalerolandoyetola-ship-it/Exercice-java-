75b5fa0c1c38823e4ffa03e2119efca59b67fe82
adewalerolandoyetola-ship-it <adewalerolandoyetola@gmail.com>
2026-03-04 07:42:22 +0100

    Add files via upload

---
+++ infopersonnelle.java
@@ -0,0 +1,13 @@
+public class InformationsPersonnelles {
+    public static void main(String[] args) {
+        String nom = "Dupont";
+        String prenom = "Jean";
+        int age = 25;
+        double taille = 1.75;
+        
+        System.out.println("Nom : " + nom);
+        System.out.println("Prénom : " + prenom);
+        System.out.println("Âge : " + age + " ans");
+        System.out.println("Taille : " + taille + " m");
+    }
+}
